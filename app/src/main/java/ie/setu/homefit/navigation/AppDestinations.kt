a962337c4535ca839ba4924850e2c5280f2e5594
Ambarish Roy <ambarishroy1996@gmail.com>
2025-04-07 22:50:01 +0100

    code added in home page

--- app/src/main/java/ie/setu/homefit/navigation/AppDestinations.kt
+++ app/src/main/java/ie/setu/homefit/navigation/AppDestinations.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.material.icons.automirrored.filled.Login
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
 
 interface AppDestination {
     val icon: ImageVector
@@ -26,3 +28,17 @@ object Register : AppDestination {
     override val label = "Register"
     override val route = "register"
 }
+object Profile : AppDestination {
+    override val icon = Icons.Default.Person
+    override val label = "Profile"
+    override val route = "profile"
+}
+object About : AppDestination {
+    override val icon = Icons.Filled.Info
+    override val label = "About"
+    override val route = "about"
+}
+val bottomAppBarDestinations = listOf(About, Profile)
+val userSignedOutDestinations = listOf(Login, Register)
+val allDestinations = listOf(
+    Home, Login, Register)
