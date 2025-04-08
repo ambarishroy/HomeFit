e459111b78de6cc8435ea7b4df5b2e23048b5821
Ambarish Roy <ambarishroy1996@gmail.com>
2025-04-08 23:57:34 +0100

    Added profile page and changed maiinactivity

---
+++ app/src/main/java/ie/setu/homefit/HomeFitApp.kt
@@ -0,0 +1,17 @@
+package ie.setu.homefit
+
+import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class HomeFitApp:Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        Timber.i("Starting DonationX Application")
+        Firebase.initialize(context = this)
+    }
+}
